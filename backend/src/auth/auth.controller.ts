8977df823c5d25d05e823cf78b712fe9e050d980
tashkeelat-ye <tashkeelat.shop@gmail.com>
2026-09-07 01:14:21 +0300

    Implement AuthController with login endpoint

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+
+import {
+  AuthService,
+} from './auth.service';
+
+@Controller('v1/auth')
+export class AuthController {
+
+  constructor(
+    private readonly auth:
+      AuthService,
+  ) {}
+
+  @Post('login')
+  login(
+    @Body()
+    body: {
+      email: string;
+      password: string;
+    },
+  ) {
+
+    return this.auth.login(
+      body.email,
+      body.password,
+    );
+  }
+}
