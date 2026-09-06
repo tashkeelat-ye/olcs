81c2628e15278bbab23b0e8cea608a767a54dc7a
tashkeelat-ye <tashkeelat.shop@gmail.com>
2026-09-07 01:23:25 +0300

    Create AppDatabase for Room persistence library

---
+++ android/app/src/main/java/com/olcs/agent/data/AppDatabase.kt
@@ -0,0 +1,18 @@
+package com.olcs.agent.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+        LocationEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase :
+    RoomDatabase() {
+
+    abstract fun locationDao():
+        LocationDao
+    }
